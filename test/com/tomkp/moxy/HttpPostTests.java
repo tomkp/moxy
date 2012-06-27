71e5b11586f498e35f197d36ec56c41730c8dd34
tomkp <tom@tomkp.com>
2012-06-27 23:44:53 +0100

    http method tests

--- test/com/tomkp/moxy/HttpPostTests.java
+++ test/com/tomkp/moxy/HttpPostTests.java
@@ -1,49 +1,53 @@
 package com.tomkp.moxy;
 
 import com.google.common.io.ByteStreams;
+import com.google.common.io.InputSupplier;
 import com.tomkp.moxy.annotations.Moxy;
 import com.tomkp.moxy.junit.MoxyRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MoxyRunner.class)
 public class HttpPostTests {
 
 
     @Test
-    @Moxy(response = "hello")
-    public void postData() throws Exception {
+    @Moxy(response = "hello post")
+    public void httpPost() throws Exception {
         URL url = new URL("http://localhost:9001");
-        post(url, "hello");
-        put(url, "hello");
-        delete(url);
+        assertEquals("hello post", post(url, "hello"));
     }
 
 
+    @Test
+    @Moxy(response = "hello put")
+    public void httpPut() throws Exception {
+        URL url = new URL("http://localhost:9001");
+        assertEquals("hello put", put(url, "hello"));
+    }
+
+
+    @Test
+    @Moxy(response = "hello delete")
+    public void httpDelete() throws Exception {
+        URL url = new URL("http://localhost:9001");
+        assertEquals("hello delete", delete(url));
+    }
+
 
     private String post(URL url, String body) throws Exception {
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-        httpURLConnection.setDoOutput(true);
-        httpURLConnection.setRequestMethod("POST");
-        OutputStreamWriter out = new OutputStreamWriter(httpURLConnection.getOutputStream());
-        out.write(body);
-        out.close();
-        return new String(ByteStreams.toByteArray(httpURLConnection.getInputStream()));
+        return write(url, body, "POST");
     }
 
 
     private String put(URL url, String body) throws Exception {
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-        httpURLConnection.setDoOutput(true);
-        httpURLConnection.setRequestMethod("PUT");
-        OutputStreamWriter out = new OutputStreamWriter(httpURLConnection.getOutputStream());
-        out.write(body);
-        out.close();
-        return new String(ByteStreams.toByteArray(httpURLConnection.getInputStream()));
+        return write(url, body, "PUT");
     }
 
 
@@ -53,6 +57,16 @@ public class HttpPostTests {
         httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
         httpURLConnection.setRequestMethod("DELETE");
         httpURLConnection.connect();
+        return new String(ByteStreams.toByteArray(httpURLConnection.getInputStream()));
+    }
+
+
+    private String write(URL url, String body, String method) throws IOException {
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod(method);
+        InputSupplier<ByteArrayInputStream> inputSupplier = ByteStreams.newInputStreamSupplier(body.getBytes());
+        ByteStreams.copy(inputSupplier, httpURLConnection.getOutputStream());
         return new String(ByteStreams.toByteArray(httpURLConnection.getInputStream()));
     }
 
