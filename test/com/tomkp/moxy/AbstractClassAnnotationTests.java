e77a946f25890b379246a243e0e704dd0ffdc8e8
tomkp <tom@tomkp.com>
2013-01-16 13:31:52 +0000

    ant test

--- test/com/tomkp/moxy/AbstractClassAnnotationTests.java
+++ test/com/tomkp/moxy/AbstractClassAnnotationTests.java
@@ -2,11 +2,13 @@ package com.tomkp.moxy;
 
 import com.tomkp.moxy.annotations.Moxy;
 import com.tomkp.moxy.junit.MoxyRunner;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 
 @RunWith(MoxyRunner.class)
 @Moxy(port = 9002)
+@Ignore
 public abstract class AbstractClassAnnotationTests {
 
 
